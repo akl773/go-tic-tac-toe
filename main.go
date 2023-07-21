b5147a88ab3c893bf1bd2f224fb4c791799d8a02
Akhil Singh <akhil.singh@infra.market>
2023-07-22 03:19:23 +0530

    abstract game initialisation

--- main.go
+++ main.go
@@ -1,45 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	ttt "github.com/akl773/go-tic-tac-toe/tictactoe"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	game := ttt.NewGame()
-	game.PrintInstruction()
-
-	player1 := ttt.NewPlayer("Player 1", 1)
-	player2 := ttt.NewPlayer("Player 2", -1)
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		game.Print()
-
-		for _, player := range []*ttt.Player{player1, player2} {
-			fmt.Printf("%s's move: ", player.Name)
-
-			inputs := make([]int, 2)
-			for i := 0; i < 2; i++ {
-				scanner.Scan()
-				input, _ := strconv.Atoi(strings.TrimSpace(scanner.Text()))
-				inputs[i] = input
-			}
-
-			if err := game.MakeMove(player, inputs[0], inputs[1]); err != nil {
-				fmt.Println(err)
-				continue
-			}
-
-			if game.CheckWin(player) {
-				fmt.Printf("%s wins!\n", player.Name)
-				game.Print()
-				os.Exit(0)
-			}
-		}
-	}
+	ttt.StartGame()
 }
--- tictactoe/tictactoe.go
+++ tictactoe/tictactoe.go
@@ -1,8 +1,12 @@
 package tictactoe
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type Game struct {
@@ -18,7 +22,7 @@ type Player struct {
 	Mark int
 }
 
-func NewGame() *Game {
+func newGame() *Game {
 	return &Game{
 		currentMark: 1, // Start with player 1's move
 	}
@@ -28,13 +32,62 @@ func NewPlayer(name string, mark int) *Player {
 	return &Player{name, mark}
 }
 
+func StartGame() {
+	game := newGame()
+	game.PrintInstruction()
+	player1 := NewPlayer("Player 1", 1)
+	player2 := NewPlayer("Player 2", -1)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		game.Print()
+
+		for _, player := range []*Player{player1, player2} {
+			fmt.Printf("%s's move (row col): ", player.Name)
+
+			scanner.Scan()
+			input := strings.TrimSpace(scanner.Text())
+			move := strings.Split(input, "")
+			if len(move) != 2 {
+				fmt.Println("Invalid input. Please enter row and column numbers separated by a space.")
+				continue
+			}
+
+			row, errRow := strconv.Atoi(move[0])
+			col, errCol := strconv.Atoi(move[1])
+
+			if errRow != nil || errCol != nil || row < 0 || row > 2 || col < 0 || col > 2 {
+				fmt.Println("Invalid input. Please enter valid row and column numbers (0, 1, or 2).")
+				continue
+			}
+
+			if err := game.MakeMove(player, row, col); err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			if game.CheckWin(player) {
+				fmt.Printf("%s wins!\n", player.Name)
+				game.Print()
+				os.Exit(0)
+			}
+
+			if game.CheckDraw() {
+				fmt.Println("It's a draw!")
+				game.Print()
+				os.Exit(0)
+			}
+		}
+	}
+}
+
 func (g *Game) MakeMove(player *Player, row int, col int) error {
 	if row < 0 || row > 2 || col < 0 || col > 2 || g.board[row][col] != 0 {
-		return errors.New("Invalid move")
+		return errors.New("invalid move")
 	}
 
 	if player.Mark != g.currentMark {
-		return errors.New("Not your turn")
+		return errors.New("not your turn")
 	}
 
 	g.board[row][col] = player.Mark
@@ -63,6 +116,18 @@ func (g *Game) CheckWin(player *Player) bool {
 	return g.diagSum[0] == winSum || g.diagSum[1] == winSum
 }
 
+func (g *Game) CheckDraw() bool {
+	movesMade := 0
+	for _, row := range g.board {
+		for _, cell := range row {
+			if cell != 0 {
+				movesMade++
+			}
+		}
+	}
+	return movesMade == 9
+}
+
 func (g *Game) Print() {
 	for _, row := range g.board {
 		for _, cell := range row {
@@ -87,7 +152,7 @@ func (g *Game) PrintInstruction() {
 	fmt.Println("The rules are simple, Player 1 is 'X' and Player 2 is 'O'.")
 	fmt.Println("Take turns to enter a row and column number (0, 1, or 2) for your mark.")
 	fmt.Println("The first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.")
-	fmt.Println("When entering your move, enter the row number and column number, separated by a space.")
+	fmt.Println("When entering your move, enter the row number and column number, without any space between them.")
 	fmt.Println("For example, '0 1' will place your mark in the top-middle cell.")
 	fmt.Println("\nHere are the coordinates of the board for your reference:")
 
